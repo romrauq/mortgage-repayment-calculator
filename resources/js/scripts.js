8431648fb007a2f46b19de5f149130ad8bd3da3a
romrauq <johnequarmor@gmail.com>
2025-01-16 13:25:57 +0000

    Assigned elements on the page to variables.

---
+++ resources/js/scripts.js
@@ -0,0 +1,12 @@
+// Assign variables references of elements on the webpage:
+const clear_button = document.getElementById("clear-button");
+const mortgage_amount_input = document.getElementById("mortgage-amount-input");
+const mortgage_term_input = document.getElementById("mortgage-term-input");
+const interest_rate_input = document.getElementById("interest-rate-input");
+const repayment_radio = document.getElementById("repayment-radio");
+const interest_only_radio = document.getElementById("interest-only-radio");
+const calculate_button = document.getElementById("calculate-button");
+const static_container = document.getElementById("static-container");
+const dynamic_container = document.getElementById("dynamic-container");
+const monthly_payments_output = document.getElementById("monthly-repayments-output");
+const total_repayment_output = document.getElementById("total-repayment-output");
